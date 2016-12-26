b1e0d96d5a1908b4230e2bf2a78e4aea32398af1
Workstation KL <raevegnforspam@rambler.ru>
2016-12-26 12:56:50 +0300

    Fix exit error

--- main.py
+++ main.py
@@ -19,18 +19,21 @@ BOT_MODE
 3 - life
 '''
 BOT_MODE = 0
-
+EXIT_MODE = False
 
 def check_updates():
     parametersList = telebot.get_updates()
+    if EXIT_MODE == True:
+        return 1
     if not parametersList:
-        return
+        return 0
     for parameters in parametersList:
         run_command(*parameters)
 
 
 def run_command(name, from_id, cmd, author_id, date):
     global BOT_MODE
+    global EXIT_MODE
     if cmd == '/ping':
         telebot.send_text(from_id, 'pong') 
 
@@ -67,13 +70,18 @@ def run_command(name, from_id, cmd, author_id, date):
     elif cmd == '/pig' and author_id in (ADMIN_ID, PIG_ID):
         import random
         telebot.send_text(from_id, 'Pig is '+random.choice(PIG_LIST))  # Answer
-    elif cmd == '/life':
-        BOT_MODE = 3
-        telebot.send_text_with_keyboard(from_id, 'Options:',
-                                        [["wake up","go sleep"],
-                                         ["breakfast","lunch","dinner"],
-                                         ["go on work", "go from work"],
-                                         ["shower","toilet_B","toilet_S"]])
+    elif cmd[0:5] == '/life':
+        if len(cmd) == 5:
+            BOT_MODE = 3
+            telebot.send_text_with_keyboard(from_id, 'Options:',
+                                            [["wake up","go sleep"],
+                                             ["breakfast","lunch","dinner"],
+                                             ["go on work", "go from work"],
+                                             ["shower","toilet_B","toilet_S"]])
+        else:
+            sqlite_add(cmd[6:], date)
+            telebot.send_text(from_id, "{0}: {1}".format(cmd[6:], human_time(date)))
+
 
 
 
@@ -87,11 +95,13 @@ def run_command(name, from_id, cmd, author_id, date):
         telebot.send_text(from_id, "Result: " + str([random.randint(1, dice_size) for i in range(number)]))
     elif cmd == '/exit':
         telebot.send_text(from_id, "Finish by user")
-        #sys.exit()
+        EXIT_MODE = True
+
     elif BOT_MODE == 3:
         sqlite_add(cmd, date)
         telebot.send_text(from_id, "{0}: {1}".format(cmd, human_time(date)))
         BOT_MODE = 0
+
     elif BOT_MODE == 1:
         d = Diary()
         if cmd == 'Day':
@@ -120,8 +130,10 @@ if __name__ == "__main__":
         try:
             
             # telebot.ping()
-            check_updates()
-            time.sleep(telebot.Interval)
+            if check_updates() != 1:
+                time.sleep(telebot.Interval)
+            else:
+                sys.exit()
         except KeyboardInterrupt:
             print 'Interrupt by user..'
             break
